1a970a33a494aea6ab7640a0dab24ce51139edd8
earth1987 <billypitchford@gmail.com>
2023-09-28 21:03:50 +0100

    Added data exploration

--- retail-customer-segmentation/config.py
+++ retail-customer-segmentation/config.py
@@ -2,8 +2,8 @@
 def file_directory(TYPE):
   """Returns file directory given file status"""
   if TYPE == 'raw':
-      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/mall-customer-segmentation/data/raw/'
+      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/retail-customer-segmentation/data/raw/'
   elif TYPE == 'cleaned':
-      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/mall-customer-segmentation/data/cleaned/'
+      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/retail-customer-segmentation/data/cleaned/'
   elif TYPE == 'processed':
-      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/mall-customer-segmentation/data/processed/'
+      return 'C:/Users/billy/OneDrive/Documents/Python Scripts/1. Portfolio/retail-customer-segmentation/data/processed/'

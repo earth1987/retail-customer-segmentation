6a7c37e12f9c60b49655535a5db6a100bf19d79d
earth1987 <billypitchford@gmail.com>
2023-10-03 20:47:09 +0100

    Added correlation functions

--- retail-customer-segmentation/custom_funcs.py
+++ retail-customer-segmentation/custom_funcs.py
@@ -1,6 +1,9 @@
 """Custom code that gets used across more than notebook"""
 import pandas as pd
 from random import sample
+import scipy
+import numpy as np
+from scipy.stats import f_oneway
 
 def perc_func(df, column):
     """Outputs the proportional breakdown of values for a given column in a dataframe."""
@@ -35,3 +38,36 @@ def perm_func(x, n_A, n_B):
     idx_A = set(sample(range(n), n_A))
     idx_B = set(range(n)) - idx_A
     return x.loc[list(idx_A)].mean() - x.loc[list(idx_B)].mean()
+
+
+def cramers_v(vars, df):
+    """Compute cramers v between categorical variables for a given dataframe df.
+    This calculation assumes each dataframe element consists of a (var1, var2) tuple.
+    """
+    var1, var2 = vars
+    contigency_table = pd.crosstab(index=df[var1],
+                                   columns=df[var2])
+    X2 = scipy.stats.chi2_contingency(contigency_table)
+    chi_stat = X2[0]
+    N = len(df)
+    min_dim = (min(contigency_table.shape)-1)
+    return np.sqrt((chi_stat/N) / min_dim)
+
+
+def oneway_anova(vars, df):
+    """Compute one way ANOVA between a categorical and numeric variable for a given dataframe df.
+    This calculation assumes each dataframe element consists of a (categorical var, numerical var) tuple.
+    """
+    cat, num = vars
+    res = f_oneway(*tuple(
+        [df[num].groupby(df[cat]).get_group(val) for val in df[cat].unique()]))
+    return res.pvalue
+
+
+def point_bs(vars, df):
+    """Compute point biserial correlation between a dichotomous categorical variable and a numeric variable for a given dataframe df.
+    This calculation assumes each dataframe element consists of a (categorical var, numerical var) tuple.
+    """
+    var1, var2 = vars
+    res = scipy.stats.pointbiserialr(df[var1], df[var2])
+    return res.statistic
